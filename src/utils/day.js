e5d04f95d5f8d6a3e6d5f75c346fcd8f3e425671
丁心雨 <932978673@qq.com>
2021-09-03 09:51:29 +0800

    封装使用day.js配置相对时间

--- src/utils/day.js
+++ src/utils/day.js
@@ -1,5 +1,5 @@
 // 使用第三方day.js时间包
-
+import Vue from 'vue'
 import dayjs from 'dayjs'
 
 //配置中文语言包
@@ -13,9 +13,13 @@ dayjs.extend(relativeTime)
 //全局使用
 dayjs.locale('zh-cn') 
 //使用例如
-console.log(dayjs().format('YYYY-MM-DD'));
+// console.log(dayjs().format('YYYY-MM-DD'));
 //使用日期格式化
 console.log(dayjs('2019-09-02').from(dayjs()));
 
+//把相对时间的代码包装成全局过滤器，然后在任何组件都可以使用
 
+Vue.filter('relativeTime',value => {
+  return dayjs(value).from(dayjs())
+})
 
